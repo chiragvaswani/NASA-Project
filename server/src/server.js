9e7eb59ffe7b59b41aa6284ca949490d5e2a0d83
chiragvaswani <chiragvaswani751@gmail.com>
2021-09-15 16:28:41 +0530

    Use functions from mongo.js to form the connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,14 @@
 const http = require("http");
-
 const app = require("./app");
 
+const { mongoConnect } = require("./services/mongo");
 const { loadPlanetsData } = require("./models/planets.model");
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
 
 async function startServer() {
+  await mongoConnect();
   await loadPlanetsData();
   server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 }
