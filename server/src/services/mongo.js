68f6e5d79d25b3eeda66c8b7ecf285d4143ed59d
chiragvaswani <chiragvaswani751@gmail.com>
2021-09-15 16:26:42 +0530

    Let's just pretend nothing happened here

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URL =
-  "mongodb+srv://admin:2nAe4tSENT8wyzs@cluster0.rq6sc.mongodb.net/nasa?retryWrites=true&w=majority";
+const MONGO_URL = "";
 
 mongoose.connection.once("open", () =>
   console.log("MongoDB connection ready!")
