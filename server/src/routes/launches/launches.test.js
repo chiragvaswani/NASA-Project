ff8f94e65c376e0a95de6c7c1cbaaac9ca2948f0
chiragvaswani <chiragvaswani751@gmail.com>
2021-09-16 14:46:10 +0530

    Fix test error

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -20,20 +20,20 @@ describe("Launches API", () => {
     const completeLaunchData = {
       mission: "USS Enterprise",
       rocket: "NCC 1701-D",
-      target: "Kepler-186",
+      target: "Kepler-62 f",
       launchDate: "January 4, 2028",
     };
 
     const launchDataWithoutDate = {
       mission: "USS Enterprise",
       rocket: "NCC 1701-D",
-      target: "Kepler-186",
+      target: "Kepler-62 f",
     };
 
     const launchDataWithInvalidDate = {
       mission: "USS Enterprise",
       rocket: "NCC 1701-D",
-      target: "Kepler-186",
+      target: "Kepler-62 f",
       launchDate: "zoot",
     };
 
